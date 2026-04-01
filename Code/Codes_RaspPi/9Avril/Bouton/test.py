b85e4164e54fe61d56e5c129003503d3d4f40cf9
AdamDubois <adamdubois19@hotmail.com>
2026-04-01 11:30:01 -0400

    Auto-commit du mer 01 avr 2026 11:30:01 EDT

--- Code/Codes_RaspPi/9Avril/Bouton/test.py
+++ Code/Codes_RaspPi/9Avril/Bouton/test.py
@@ -8,7 +8,6 @@ bouton.start()
 try:
     while not bouton.gagnee:
         bouton.play()
-        time.sleep(0.01) # Petit delay pour éviter de surcharger le CPU, peut être ajusté selon les besoins
 except Exception as e:
     logger.error(f"Erreur inattendue dans le programme principal: {e}")
 except KeyboardInterrupt:
