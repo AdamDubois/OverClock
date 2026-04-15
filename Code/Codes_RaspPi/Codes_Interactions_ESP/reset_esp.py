547f980a8d09d1236b4223e02c8a73a8a1e6672a
AdamDubois <adamdubois19@hotmail.com>
2026-04-15 10:30:01 -0400

    Auto-commit du mer 15 avr 2026 10:30:01 EDT

--- Code/Codes_RaspPi/Codes_Interactions_ESP/reset_esp.py
+++ Code/Codes_RaspPi/Codes_Interactions_ESP/reset_esp.py
@@ -1,13 +1,28 @@
 # Met la pin de reset des esp a 0
 import RPi.GPIO as GPIO
-rest_pin = 36
+# Pin de reset pour les ESPs
+reset_pin_RFID = 36
+reset_pin_IO = 32
+reset_pin_NEO = 22
+
+# Correspondance des GPIO physiques pour les pins de reset
+reset_GPIO_RFID = 16
+reset_GPIO_IO = 12
+reset_GPIO_NEO = 25
 
 try:
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(rest_pin, GPIO.OUT)
-    GPIO.output(rest_pin, GPIO.LOW)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(reset_pin_IO, GPIO.OUT)
+    GPIO.setup(reset_pin_RFID, GPIO.OUT)
+    GPIO.setup(reset_pin_NEO, GPIO.OUT)
     print("Pin de reset mise à LOW pour réinitialiser les ESPs.")
+    while True:
+        GPIO.output(reset_pin_IO, GPIO.LOW)
+        GPIO.output(reset_pin_RFID, GPIO.LOW)
+        GPIO.output(reset_pin_NEO, GPIO.LOW)
 except Exception as e:
     print(f"Erreur lors de la configuration de la pin de reset: {e}")
+except KeyboardInterrupt:
+    print("Interruption par l'utilisateur. Nettoyage des GPIO.")
 finally:
     GPIO.cleanup()
